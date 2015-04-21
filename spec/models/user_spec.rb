0d87db505dda3017593de1a5a5e90812f52427cf
yossan4343434 <yossan4343434@gmail.com>
2015-04-21 13:30:55 +0900

    Add test to search downcase email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,13 +21,13 @@ describe User do
 		it { should_not be_valid }
 	end
 
-	describe "when email is not present" do
-		before { @user.email = " " }
+	describe "when name is too long" do
+		before { @user.name = "a" * 51 }
 		it { should_not be_valid }
 	end
 
-	describe "when name is too long" do
-		before { @user.name = "a" * 51 }
+	describe "when email is not present" do
+		before { @user.email = " " }
 		it { should_not be_valid }
 	end
 
@@ -38,6 +38,16 @@ describe User do
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
 			end
+		end
+	end
+
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExaAAA.COm" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
 		end
 	end
 
